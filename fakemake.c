be14b5c3c31dcb7bb07c80fb00cf7f95c1c2fc56
Madelyn Gross <mgross10@hydra8.eecs.utk.edu>
2025-02-18 13:51:59 -0500

    initial commit

---
+++ fakemake.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "fields.h"
+#include "jval.h"//unsure if needed
+#include "dllist.h"//unsure if needed
+#include "jrb.h"//unsure if needed
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
